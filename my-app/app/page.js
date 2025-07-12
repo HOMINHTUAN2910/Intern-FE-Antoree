8b7fa28cbc09df000840059ba385412aeafbbd63
HOMINHTUAN2910 <tuanhm2910@gmail.com>
2025-07-12 22:05:18 +0700

    Card commit

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
     <div className="bg-black">
       <Navbar />
       <Hero />
+      <Card />
       <Footer />
     </div>
   );
